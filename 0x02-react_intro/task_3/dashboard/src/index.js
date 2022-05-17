ecf04f7ffe7a751b1068303fa2c5a7e398e05171
faykris <faykris28@gmail.com>
2022-05-17 11:21:14 -0500

    [Add] test files for task_3 folder: verify with checker

--- 0x02-react_intro/task_3/dashboard/src/index.js
+++ 0x02-react_intro/task_3/dashboard/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import Notifications from './Notifications'
+import Notifications from './Notifications';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
