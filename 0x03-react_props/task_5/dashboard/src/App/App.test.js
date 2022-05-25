2112ccf51dda2b5d6effc79895dda049995a812d
faykris <faykris28@gmail.com>
2022-05-24 23:35:28 -0500

    [Update] App.test.js: add 2 test cases to the suit

--- 0x03-react_props/task_5/dashboard/src/App/App.test.js
+++ 0x03-react_props/task_5/dashboard/src/App/App.test.js
@@ -8,7 +8,12 @@ describe('<App />', () => {
     expect(wrapper.exists()).toEqual(true);
   });
 
-  it('if <App /> renders when CourseList is not displayed', () => {
+  it('if <App /> renders Login when isLoggedIn property is not defined', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find('Login')).toHaveLength(1);
+  });
+
+  it('if <App /> renders Login when CourseList is not displayed', () => {
     const wrapper = shallow(<App isLoggedIn={false}/>);
     expect(wrapper.find('Login')).toHaveLength(1);
   });
@@ -21,5 +26,15 @@ describe('<App />', () => {
   it('if <App /> when isLoggedIn is true, CourseList component is included', () => {
     const wrapper = shallow(<App isLoggedIn={true}/>);
     expect(wrapper.find('CourseList')).toHaveLength(1);
+  });
+
+  it('if <App /> when isLoggedIn is true, Header component is included', () => {
+    const wrapper = shallow(<App isLoggedIn={true}/>);
+    expect(wrapper.find('Header')).toHaveLength(1);
+  });
+
+  it('if <App /> when isLoggedIn is true, Footer component is included', () => {
+    const wrapper = shallow(<App isLoggedIn={true}/>);
+    expect(wrapper.find('Footer')).toHaveLength(1);
   });
 });
