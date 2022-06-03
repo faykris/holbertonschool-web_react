ec54858c5d06f0393c8a1fa18e543a89a33d04a3
faykris <faykris28@gmail.com>
2022-06-02 19:22:50 -0500

    [Add] task 1 files: verify with checker

---
+++ 0x05-React_inline_styling/task_1/dashboard/src/BodySection/BodySection.js
@@ -0,0 +1,41 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { StyleSheet, css } from 'aphrodite';
+
+const styles = StyleSheet.create({
+  bodySection: {
+    display: "flex",
+    flexWrap: "wrap",
+    width: "100%",
+  },
+
+  h2: {
+    width: "100%",
+  },
+})
+
+class BodySection extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const { children, title } = this.props;
+    return (
+      <div className={css(styles.bodySection)}>
+        <h2 className={css(styles.h2)}>{title}</h2>
+        {children}
+      </div>
+    );
+  }
+}
+
+BodySection.defaultProps = {
+  title: "",
+};
+
+BodySection.propTypes = {
+  title: PropTypes.string,
+};
+
+export default BodySection;
