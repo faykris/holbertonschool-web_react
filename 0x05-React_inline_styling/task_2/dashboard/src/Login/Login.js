9909dcf871d55ba811e4f621e677e4a78a5f4ad3
faykris <faykris28@gmail.com>
2022-06-02 19:47:25 -0500

    [Add] task 2 files: verify with checker

---
+++ 0x05-React_inline_styling/task_2/dashboard/src/Login/Login.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Login() {
+  return (
+    <div className="login">
+      <p>Login to access the full dashboard</p>
+      <form>
+        <label htmlFor="email">Email: </label>
+        <input type="text" id="email" name="email" />
+        <label htmlFor="email">Password: </label>
+        <input type="password" id="password" name="password" />
+        <button>OK</button>
+      </form>
+    </div>
+  );
+}
+
+export default Login;
