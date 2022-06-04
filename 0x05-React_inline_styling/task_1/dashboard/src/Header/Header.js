62bf1d3d58b676393e4aa0e8c481b6f980e8e12a
faykris <faykris28@gmail.com>
2022-06-03 20:27:24 -0500

    [update] task 1 files: trying with new aphrodite elements

--- 0x05-React_inline_styling/task_1/dashboard/src/Header/Header.js
+++ 0x05-React_inline_styling/task_1/dashboard/src/Header/Header.js
@@ -9,13 +9,19 @@ const styles = StyleSheet.create({
   h1: {
     color: 'red',
   },
+
+  header: {
+    display: 'flex',
+    alignItems: 'center',
+    borderBottom: '3px solid red',
+  }
 })
 function Header() {
   return (
-    <>
+    <div className={css(styles.header)}>
       <img src={logo} alt="hbtn-logo"  className={css(styles.img)} />
       <h1 className={css(styles.h1)} >School dashboard</h1>
-    </>
+    </div>
   );
 }
 
